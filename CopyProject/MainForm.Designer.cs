0be4ad6bb1a47571ee74b9c681d271d5850cffab
Administrator <Administrator@XIAOHONG>
2025-07-11 16:23:43 +0800

    添加项目文件。

---
+++ CopyProject/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CopyProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtSourcePath = new System.Windows.Forms.TextBox();
+            this.BtnSelectSource = new System.Windows.Forms.Button();
+            this.BtnCopyProject = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVersion = new System.Windows.Forms.Label();
+            this.txtAuthor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSourcePath
+            // 
+            this.txtSourcePath.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtSourcePath.Location = new System.Drawing.Point(12, 30);
+            this.txtSourcePath.Name = "txtSourcePath";
+            this.txtSourcePath.Size = new System.Drawing.Size(400, 25);
+            this.txtSourcePath.TabIndex = 0;
+            // 
+            // BtnSelectSource
+            // 
+            this.BtnSelectSource.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BtnSelectSource.Location = new System.Drawing.Point(418, 30);
+            this.BtnSelectSource.Name = "BtnSelectSource";
+            this.BtnSelectSource.Size = new System.Drawing.Size(30, 25);
+            this.BtnSelectSource.TabIndex = 2;
+            this.BtnSelectSource.Text = "…";
+            this.BtnSelectSource.UseVisualStyleBackColor = true;
+            this.BtnSelectSource.Click += new System.EventHandler(this.BtnSelectSource_Click);
+            // 
+            // BtnCopyProject
+            // 
+            this.BtnCopyProject.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.BtnCopyProject.Location = new System.Drawing.Point(470, 30);
+            this.BtnCopyProject.Name = "BtnCopyProject";
+            this.BtnCopyProject.Size = new System.Drawing.Size(100, 25);
+            this.BtnCopyProject.TabIndex = 9;
+            this.BtnCopyProject.Text = "复制项目";
+            this.BtnCopyProject.UseVisualStyleBackColor = true;
+            this.BtnCopyProject.Click += new System.EventHandler(this.BtnCopyProject_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "选择要复制的源项目";
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.AutoSize = true;
+            this.txtVersion.Location = new System.Drawing.Point(436, 101);
+            this.txtVersion.Name = "txtVersion";
+            this.txtVersion.Size = new System.Drawing.Size(134, 15);
+            this.txtVersion.TabIndex = 11;
+            this.txtVersion.Text = "Version：1.0.0.0";
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.AutoSize = true;
+            this.txtAuthor.Location = new System.Drawing.Point(388, 125);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(182, 15);
+            this.txtAuthor.TabIndex = 12;
+            this.txtAuthor.Text = "Author：ZhangHongShuai";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(400, 50);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Copy Project 可以尝试复制正在运行中的WinCC项目\r\n但不包括历史归档报警等！";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 120);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(200, 25);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(218, 125);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(76, 15);
+            this.labelProgress.TabIndex = 15;
+            this.labelProgress.Text = "准备中...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(582, 153);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtVersion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCopyProject);
+            this.Controls.Add(this.txtSourcePath);
+            this.Controls.Add(this.BtnSelectSource);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Copy Project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSourcePath;
+        private System.Windows.Forms.Button BtnSelectSource;
+        private System.Windows.Forms.Button BtnCopyProject;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtVersion;
+        private System.Windows.Forms.Label txtAuthor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelProgress;
+    }
+}
+
---
+++ CopyProject/MainForm.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyProject
+{
+    public partial class MainForm : Form
+    {
+        const string ConnString = "Server=.\\WINCC;Database=master;Integrated Security=true;";
+        public MainForm()
+        {
+            InitializeComponent();
+            txtVersion.Text = "Version：" + Application.ProductVersion;
+        }
+        private void BtnSelectSource_Click(object sender, EventArgs e)
+        {
+            using var dialog = new OpenFileDialog
+            {
+                Filter = "WinCC Project (*.mcp)|*.mcp|All files (*.*)|*.*",
+                Title = "选择 WinCC 项目文件"
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSourcePath.Text = dialog.FileName;
+            }
+        }
+        private async void BtnCopyProject_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using var dialog = new FolderBrowserDialog
+                {
+                    Description = "选择目标文件夹"
+                };
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var sourceDir = Path.GetDirectoryName(txtSourcePath.Text) ?? throw new InvalidOperationException("未指定来源目录");
+                    var targetRoot = dialog.SelectedPath;
+                    var projectName = Path.GetFileNameWithoutExtension(txtSourcePath.Text);
+                    var destDir = Path.Combine(targetRoot, new DirectoryInfo(sourceDir).Name);
+
+                    var totalFiles = CountFiles(sourceDir);
+                    var copiedFiles = 0;
+
+                    var progress = new Progress<int>(value =>
+                    {
+                        int percent = (int)((value * 100.0) / totalFiles);
+                        progressBar1.Value = percent;
+                        labelProgress.Text = $"{value}/{totalFiles} ({percent}%)";
+                    });
+
+                    await Task.Run(() =>
+                    {
+                        CopyDirectory(sourceDir, destDir, ref copiedFiles, totalFiles, (current, _) => ((IProgress<int>)progress).Report(current));
+                    });
+                    CopyDatabase(projectName, sourceDir, destDir);
+                    CopyDatabase(projectName + "RT", sourceDir, destDir);
+
+                    if (MessageBox.Show("复制完成！是否归档为 ZIP？", "确认", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                    {
+                        ZipFile.CreateFromDirectory(destDir, $"{destDir}_{DateTime.Now:yyyyMMdd_HHmmss}.zip", CompressionLevel.Fastest, includeBaseDirectory: false);
+                        MessageBox.Show("归档完成！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"发生错误：{ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void CopyDatabase(string baseName, string sourceDir, string destDir)
+        {
+            var mdfPath = Path.Combine(sourceDir, baseName + ".mdf");
+            var ldfPath = Path.Combine(sourceDir, baseName + ".ldf");
+
+            if (!IsMdfAttached(mdfPath))
+            {
+                File.Copy(mdfPath, Path.Combine(destDir, Path.GetFileName(mdfPath)), overwrite: true);
+                File.Copy(ldfPath, Path.Combine(destDir, Path.GetFileName(ldfPath)), overwrite: true);
+
+                return;
+            }
+
+            var dbName = GetDatabaseName(mdfPath);
+            var bakFile = Path.Combine(destDir, $"{dbName}_{DateTime.Now:yyyyMMdd_HHmmss}.bak");
+            BackupDatabase(dbName, bakFile);
+            RestoreDatabase(baseName, bakFile);
+
+            File.Delete(bakFile);
+        }
+        private bool IsMdfAttached(string mdfPath)
+        {
+            const string sql = @"SELECT COUNT(*) FROM sys.master_files WHERE LOWER(physical_name) = LOWER(@mdf);";
+
+            using (var cn = new SqlConnection(ConnString))
+            using (var cmd = new SqlCommand(sql, cn))
+            {
+                cmd.Parameters.AddWithValue("@mdf", mdfPath);
+                cn.Open();
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+        private string GetDatabaseName(string mdfPath)
+        {
+            const string sql = @"SELECT DB_NAME(database_id) FROM sys.master_files WHERE LOWER(physical_name) = LOWER(@mdf);";
+
+            using (var cn = new SqlConnection(ConnString))
+            using (var cmd = new SqlCommand(sql, cn))
+            {
+                cmd.Parameters.AddWithValue("@mdf", mdfPath);
+                cn.Open();
+                object obj = cmd.ExecuteScalar();
+                if (obj == null || obj == DBNull.Value)
+                    throw new Exception("在 sys.master_files 中未找到对应的 mdf 路径。");
+                return (string)obj;
+            }
+        }
+        private void BackupDatabase(string dbName, string bakFile)
+        {
+            string sql = $@"BACKUP DATABASE [{dbName}] TO DISK = @bak WITH INIT, COPY_ONLY;";
+
+            using (var cn = new SqlConnection(ConnString))
+            using (var cmd = new SqlCommand(sql, cn))
+            {
+                cmd.Parameters.AddWithValue("@bak", bakFile);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        private void RestoreDatabase(string baseName, string bakPath)
+        {
+            // 读出逻辑名
+            const string fileListSql = "RESTORE FILELISTONLY FROM DISK = @bak";
+            string logicalData = null, logicalLog = null;
+            using (var cn = new SqlConnection(ConnString))
+            using (var cmd = new SqlCommand(fileListSql, cn))
+            {
+                cmd.Parameters.AddWithValue("@bak", bakPath);
+                cn.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var type = reader["Type"].ToString();
+                        if (type == "D") logicalData = reader["LogicalName"].ToString();
+                        else if (type == "L") logicalLog = reader["LogicalName"].ToString();
+                    }
+                }
+            }
+            if (logicalData == null || logicalLog == null)
+                throw new InvalidOperationException("无法从备份中获取逻辑文件名");
+
+            // 生成临时库名并还原
+            var tempDb = "Temp_" + Guid.NewGuid().ToString("N");
+            var dataDest = Path.Combine(Path.GetDirectoryName(bakPath), baseName + ".mdf");
+            var logDest = Path.Combine(Path.GetDirectoryName(bakPath), baseName + ".ldf");
+            var restoreSql = $@"
+                RESTORE DATABASE [{tempDb}]
+                    FROM DISK = @bak
+                    WITH
+                    MOVE @logicalData TO @dataDest,
+                    MOVE @logicalLog  TO @logDest,
+                    RECOVERY, REPLACE;";
+
+            using (var cn = new SqlConnection(ConnString))
+            using (var cmd = new SqlCommand(restoreSql, cn))
+            {
+                cmd.Parameters.AddWithValue("@bak", bakPath);
+                cmd.Parameters.AddWithValue("@logicalData", logicalData);
+                cmd.Parameters.AddWithValue("@logicalLog", logicalLog);
+                cmd.Parameters.AddWithValue("@dataDest", dataDest);
+                cmd.Parameters.AddWithValue("@logDest", logDest);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            // 第三步：卸载临时库
+            using (var cn = new SqlConnection(ConnString))
+            using (var cmd = new SqlCommand("EXEC sp_detach_db @db, 'true';", cn))
+            {
+                cmd.Parameters.AddWithValue("@db", tempDb);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        private int CountFiles(string sourceDir)
+        {
+            int count = 0;
+
+            foreach (string file in Directory.GetFiles(sourceDir))
+            {
+                string ext = Path.GetExtension(file).ToLower();
+                if (ext != ".mdf" && ext != ".ldf" && ext != ".lck")
+                    count++;
+            }
+
+            foreach (string dir in Directory.GetDirectories(sourceDir))
+            {
+                count += CountFiles(dir);
+            }
+
+            return count;
+        }
+        private void CopyDirectory(string sourceDir, string targetDir, ref int copied, int total, Action<int, int> progressCallback)
+        {
+            Directory.CreateDirectory(targetDir);
+
+            foreach (string file in Directory.GetFiles(sourceDir))
+            {
+                string ext = Path.GetExtension(file).ToLower();
+                if (ext != ".mdf" && ext != ".ldf" && ext != ".lck")
+                {
+                    string targetFile = Path.Combine(targetDir, Path.GetFileName(file));
+                    File.Copy(file, targetFile, true);
+                    copied++;
+                    progressCallback?.Invoke(copied, total);
+                }
+            }
+
+            foreach (string dir in Directory.GetDirectories(sourceDir))
+            {
+                string targetSubDir = Path.Combine(targetDir, Path.GetFileName(dir));
+                CopyDirectory(dir, targetSubDir, ref copied, total, progressCallback);
+            }
+        }
+    }
+}
